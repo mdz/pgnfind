62a44a8fff58079cd65ae6c3a9feaf3121dec3bb
mdz <mdz>
1999-04-14 04:22:57 +0000

    Now contains a stub for testing the algebraic notation parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream.h>
-#include <ChessPosition.h>
+#include "ChessMove.h"
 
 int main(int argc, char *argv[]) {
   //char buf[1024] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-  char buf[1024] = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KkqQ e3 0 1";
-  ChessPosition pos( buf );
+  //  char buf[1024] = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KkqQ e3 0 1";
+  //  ChessPosition pos( buf );
 
-  pos.write_FEN( buf );
-  cout << "FEN: " << buf << endl;
+//    pos.write_FEN( buf );
+//    cout << "FEN: " << buf << endl;
 
-  pos.set_piece_at( "a2", ChessPiece( ChessPiece::Rook, ChessPiece::Black ) );
+//    pos.set_piece_at( "a2", ChessPiece( ChessPiece::Rook, ChessPiece::Black ) );
 
-  pos.write_FEN( buf );
-  cout << "FEN: " << buf << endl;
+//    pos.write_FEN( buf );
+//    cout << "FEN: " << buf << endl;
 
-  cout << "Piece at a1 is: " << pos.get_piece_at( "a1" ).get_type()
-       << ',' << pos.get_piece_at( "a1" ).get_color() << endl;
+//    cout << "Piece at a1 is: " << pos.get_piece_at( "a1" ).get_type()
+//    << ',' << pos.get_piece_at( "a1" ).get_color() << endl;
+
+  char buf[1024];
+
+  while (1) {
+    cin.getline( buf, 1024 );
+    ChessMove move( buf, ChessMove::Algebraic, NULL );
+  }
 }
