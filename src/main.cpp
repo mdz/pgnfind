44d0f0c4eb9b2d2a9581ea2ca3ad5a469775cdb5
mdz <mdz>
1999-04-15 03:31:52 +0000

    Now catches exceptions from ChessMove

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,22 @@ int main(int argc, char *argv[]) {
 //    << ',' << pos.get_piece_at( "a1" ).get_color() << endl;
 
   char buf[1024];
+  ChessGame game;
 
   while (1) {
     cin.getline( buf, 1024 );
-    ChessMove move( buf, ChessMove::Algebraic, NULL );
+    try {
+      ChessMove move( buf, ChessMove::Algebraic, &game );
+    }
+
+    catch (ChessMove::Exception cme) {
+      if ( cme == ChessMove::InvalidMove ) {
+	cout << "Invalid move" << endl;
+      } else if ( cme == ChessMove::IllegalMove ) {
+	cout << "Illegal move" << endl;
+      } else {
+	cout << "Unrecognized exception" << endl;
+      }
+    }
   }
 }
