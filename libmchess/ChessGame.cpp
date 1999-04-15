0e3114afea048a612ebd45a0838d8221acf5a5cf
mdz <mdz>
1999-04-15 03:59:33 +0000

    Implementation for ChessGame

---
+++ libmchess/ChessGame.cpp
@@ -0,0 +1,21 @@
+#include "ChessGame.h"
+
+ChessGame::ChessGame() {
+  int i;
+  for( i = 0 ; i < CHESSGAME_MAX_MOVES ; ++i ) {
+    moves[i] = NULL;
+  }
+}
+
+ChessGame::ChessGame( const char *pgn ) {
+  ChessGame();
+}
+
+ChessGame::ChessGame( const ChessPosition &pos ) {
+  ChessGame();
+  position = pos;
+}
+
+const ChessPosition &ChessGame::current_position() const {
+  return position;
+}
