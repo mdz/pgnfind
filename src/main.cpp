0fac85801de554cd12b17ec17fb21a1292ee7282
mdz <mdz>
1999-04-17 21:44:43 +0000

    Now contains a loop that reads moves and prints resulting FENs incrementally

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,19 @@
 #include "ChessMove.h"
 
 int main(int argc, char *argv[]) {
-  //char buf[1024] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-  //  char buf[1024] = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KkqQ e3 0 1";
-  //  ChessPosition pos( buf );
-
-//    pos.write_FEN( buf );
-//    cout << "FEN: " << buf << endl;
-
-//    pos.set_piece_at( "a2", ChessPiece( ChessPiece::Rook, ChessPiece::Black ) );
-
-//    pos.write_FEN( buf );
-//    cout << "FEN: " << buf << endl;
-
-//    cout << "Piece at a1 is: " << pos.get_piece_at( "a1" ).get_type()
-//    << ',' << pos.get_piece_at( "a1" ).get_color() << endl;
-
   char buf[1024];
-  ChessGame game;
+  ChessGame game;/*( ChessPosition(
+		   "rnbqkbnr/pppppppp/8/8/8/8/8/RNBQKBNR w KQkq - 0 1" ) );*/
 
   while (1) {
     cin.getline( buf, 1024 );
     try {
       ChessMove move( buf, ChessMove::Algebraic, &game );
+
+      game.make_move( move );
+
+      game.current_position().write_FEN( buf );
+      cout << buf << endl;
     }
 
     catch (ChessMove::Exception cme) {
@@ -35,5 +26,6 @@ int main(int argc, char *argv[]) {
 	cout << "Unrecognized exception" << endl;
       }
     }
+
   }
 }
