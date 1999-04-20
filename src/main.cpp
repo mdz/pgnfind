d05ff394d315ab79abec43da6cd905255b532804
mdz <mdz>
1999-04-20 00:15:37 +0000

    Now contains a loop for testing the reading of entire PGN files

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
 #include <iostream.h>
 #include "ChessMove.h"
+#include "pgn_parse_int.h"
 
 int main(int argc, char *argv[]) {
-  char buf[1024];
-  ChessGame game;/*( ChessPosition(
-		   "rnbqkbnr/pppppppp/8/8/8/8/8/RNBQKBNR w KQkq - 0 1" ) );*/
+  char buf[1024] = "";
+  char *ptr = buf;
 
-  while (1) {
-    cin.getline( buf, 1024 );
-    try {
-      ChessMove move( buf, ChessMove::Algebraic, &game );
-
-      game.make_move( move );
-
-      game.current_position().write_FEN( buf );
-      cout << buf << endl;
-    }
-
-    catch (ChessMove::Exception cme) {
-      if ( cme == ChessMove::InvalidMove ) {
-	cout << "Invalid move" << endl;
-      } else if ( cme == ChessMove::IllegalMove ) {
-	cout << "Illegal move" << endl;
-      } else {
-	cout << "Unrecognized exception" << endl;
-      }
-    }
-
+  int len = 0;
+  while ( read(fileno(stdin), ptr, 1024 - len ) > 0 ) {
+    len = strlen( buf );
+    ptr = buf + len;
   }
+
+  try {
+    ChessGame game( buf );
+    
+    game.current_position().write_FEN( buf );
+    cout << buf << endl;
+  }
+  
+  catch (ChessMove::Exception cme) {
+    if ( cme == ChessMove::InvalidMove ) {
+      cout << "Invalid move" << endl;
+    } else if ( cme == ChessMove::IllegalMove ) {
+      cout << "Illegal move" << endl;
+    } else {
+      cout << "Unrecognized exception" << endl;
+    }
+
+    return 1;
+  }
+
+  return 0;
 }
